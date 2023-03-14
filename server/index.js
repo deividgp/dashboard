f883d38b2138e67c4bc2decf5716ff6d2898c8c7
Deividgp <dayd4@outlook.com>
2023-03-14 15:11:40 +0100

    Add about.json route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,59 @@
-console.log('Hello world')
+import express from 'express'
+
+const app = express()
+
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: 'dist' })
+})
+
+app.get('/about.json', (req, res) => {
+  const about = {
+    client: {
+      host: req.socket.remoteAddress
+    },
+    server: {
+      current_time: Math.floor(Date.now() / 1000),
+      services: [
+        {
+          name: 'weather',
+          widgets: [
+            {
+              name: 'city_temperature',
+              description: 'Display temperature for a city',
+              params: [
+                {
+                  name: 'city',
+                  type: 'string'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'rss',
+          widgets: [
+            {
+              name: 'article_list',
+              description: 'Displaying the list of the last articles',
+              params: [
+                {
+                  name: ' link ',
+                  type: ' string '
+                },
+                {
+                  name: ' number ',
+                  type: ' integer '
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+  res.json(about)
+})
+
+app.listen(8080)
