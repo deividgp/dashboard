fb9924f0db1859c71532ba2c70b6571b167b5c51
deividgp <dayd4@outlook.com>
2023-03-18 19:25:42 +0100

    Add dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Button, FormControl } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { WidthProvider, Responsive } from 'react-grid-layout'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
 import './App.css'
+import Weather from './widgets/Weather'
+const ResponsiveReactGridLayout = WidthProvider(Responsive)
+const removeStyle = {
+  position: 'absolute',
+  right: '2px',
+  top: 0,
+  cursor: 'pointer'
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [items, setItems] = useState([])
+  const [widget, setWidget] = useState('')
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'))
+    if (items) {
+      setItems(items)
+      setCounter(items[items.length - 1].i + 1)
+    }
+  }, [])
+
+  const handleSelectChange = (e) => {
+    setWidget(e.target.value)
+  }
+
+  const getWidget = (el) => {
+    switch (el.widget) {
+      case 'weather':
+        return <Weather id={el.i} />
+    }
+  }
+
+  const createElement = (el) => {
+    const i = el.i
+    return (
+      <div key={i} data-grid={el}>
+        <span className='text'>{getWidget(el)}</span>
+        <span
+          className='remove'
+          style={removeStyle}
+          onClick={() => handleRemoveItem(i)}
+        >
+          X
+        </span>
+      </div>
+    )
+  }
+
+  const handleAddWidget = () => {
+    if (widget === '') return
+
+    setItems(
+      items.concat({
+        i: counter,
+        widget,
+        x: (items.length * 2) % 12,
+        y: 5555,
+        w: 2,
+        h: 2
+      })
+    )
+    localStorage.setItem(
+      'items',
+      JSON.stringify(
+        items.concat({
+          i: counter,
+          widget,
+          x: (items.length * 2) % 12,
+          y: 5555,
+          w: 2,
+          h: 2
+        })
+      )
+    )
+    setCounter(counter + 1)
+  }
+
+  const handleRemoveItem = (i) => {
+    setItems(items.filter((item) => item.i !== i))
+    localStorage.setItem(
+      'items',
+      JSON.stringify(items.filter((item) => item.i !== i))
+    )
+  }
+
+  const clearStorage = () => {
+    localStorage.clear()
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Button onClick={clearStorage}>Clear local storage</Button>
+      <Box sx={{ width: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id='demo-simple-select-label'>Widget</InputLabel>
+          <Select
+            labelId='demo-simple-select-label'
+            id='demo-simple-select'
+            value={widget}
+            label='Widget'
+            onChange={handleSelectChange}
+          >
+            <MenuItem value='weather'>Weather</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Button onClick={handleAddWidget}>Add Widget</Button>
+      <ResponsiveReactGridLayout
+        className='layout'
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={100}
+      >
+        {items.map((el) => createElement(el))}
+      </ResponsiveReactGridLayout>
+    </>
   )
 }
 
---
+++ client/src/widgets/Weather.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from 'react'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import { Input } from '@mui/material'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+}
+
+function Weather(props) {
+  const intervalRef = useRef(0)
+  const [open, setOpen] = useState(false)
+  const [city, setCity] = useState('')
+  const [auxCity, setAuxCity] = useState('')
+  const [currentTemp, setCurrentTemp] = useState(0)
+
+  useEffect(() => {
+    if (props.id === undefined) return
+
+    const auxCity2 = JSON.parse(localStorage.getItem('0'))
+    if (auxCity2) {
+      setCity(auxCity2)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (city === '') return
+    localStorage.setItem(props.id, JSON.stringify(auxCity))
+    setOpen(false)
+    clearInterval(intervalRef)
+    fetchInterval()
+    intervalRef.current = setInterval(fetchInterval, 60000)
+  }, [city])
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const handleChange = (e) => {
+    setAuxCity(e.target.value)
+  }
+  const fetchInterval = () => {
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + city, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '4ba3df7148msh60f245f710ad9cfp13627bjsnf58013f30f59',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    })
+      .then((res) => res.json())
+      .then((json) => setCurrentTemp(json.current.temp_c))
+      .catch((err) => console.error('error:' + err))
+  }
+
+  const handleAccept = () => {
+    setCity(auxCity)
+  }
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Set up</Button>
+      <br />
+      <span>{city}</span>
+      <br />
+      {city !== '' && <span>{currentTemp} C</span>}
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Input value={auxCity} placeholder='City' onChange={handleChange} />
+          <Button onClick={handleAccept}>Accept</Button>
+        </Box>
+      </Modal>
+    </>
+  )
+}
+
+export default Weather
